4731a7c9a2fae3607fd0e90214ea2893db0a626c
100rabhSG <saurabh.agrawal.1605@gmail.com>
2026-04-13 02:30:20 +0530

    Revert "intentional TS error to verify CI blocks merge"
    This reverts commit 02a916a40d780ec7fbbffa9f9dc802062dd65c76.
    
    revert error check

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,6 +4,3 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-
-// Intentional TS error to test CI
-const x: number = "this is not a number"
