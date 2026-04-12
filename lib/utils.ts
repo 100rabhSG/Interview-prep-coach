02a916a40d780ec7fbbffa9f9dc802062dd65c76
100rabhSG <saurabh.agrawal.1605@gmail.com>
2026-04-13 02:28:00 +0530

    intentional TS error to verify CI blocks merge

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,6 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+// Intentional TS error to test CI
+const x: number = "this is not a number"
